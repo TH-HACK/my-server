4799066a708bf1ad389d6940b1e83c34079852cb
TECHNO HACK <104524437+TH-HACK@users.noreply.github.com>
2024-08-27 21:45:40 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/get-url', methods=['GET'])
 def get_url():
-    return jsonify({"url": "https://your-vercel-project.vercel.app"})
+    return jsonify({"url": "https://my-server-lilac.vercel.app/"})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
